b45fa76f9957b7d50a024958d980ac89886bc481
Raaffii <rraaafffii@gmail.com>
2026-01-30 15:02:59 +0700

    fix

--- client/src/pages/TeacherPage.jsx
+++ client/src/pages/TeacherPage.jsx
@@ -89,6 +89,7 @@ export default function TeacherPage() {
           onClick: () => {
             setIsModalOpen(true);
             setModalMode("create");
+            setSelectedTeacher({});
           },
         }}
         showSearch={true}
